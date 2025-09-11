a3cdf3b70b2e24a99fe2c8da24445a57cef9262f
Nadeŭka <me@nadevko.cc>
2025-09-11 22:53:34 +0300

    setup build env

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    n = {
+      url = "path:/home/nadevko/Workspace/nabiki";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      treefmt-nix,
+      n,
+      ...
+    }@inputs:
+    n nixpkgs.legacyPackages.x86_64-linux.dotnet-sdk_9.meta.platforms (
+      platform:
+      let
+        treefmt = treefmt-nix.lib.evalModule pkgs {
+          programs.nixfmt.enable = true;
+          programs.nixfmt.strict = true;
+          programs.prettier.enable = true;
+          programs.cmake-format.enable = true;
+        };
+        pkgs = nixpkgs.legacyPackages.${platform};
+      in
+      {
+        devShells.default = pkgs.mkShell.override { inherit (pkgs.llvmPackages) stdenv; } rec {
+          packages = with pkgs; [
+            llvmPackages.lldb
+            sdl3
+            sdl3.dev
+            cmake
+          ];
+          LD_LIBRARY_PATH = nixpkgs.lib.makeLibraryPath packages;
+          vscode-settings = pkgs.writeText "settings.json" (
+            builtins.toJSON { "clangd.path" = "${pkgs.clang-tools}/bin/clangd"; }
+          );
+          shellHook = ''
+            mkdir .vscode
+            cat ${vscode-settings} > .vscode/settings.json
+          '';
+        };
+        formatter = treefmt.config.build.wrapper;
+        checks.treefmt = treefmt.config.build.wrapper;
+        packages = n.lib.readPackages {
+          path = ./pkgs;
+          overrides.inputs = inputs;
+          inherit pkgs;
+        };
+        legacyPackages = n.lib.readLegacyPackages {
+          path = ./pkgs;
+          overrides.inputs = inputs;
+          inherit pkgs;
+        };
+      }
+    );
+}
---
+++ src/main.c
@@ -0,0 +1,5 @@
+int main() {
+    short a = 2;
+    short b = a + a;
+    return 0;
+}
