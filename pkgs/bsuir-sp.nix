2edb5f11a4b291c4dc8bab9f03176e455ce2b671
Nadeŭka <me@nadevko.cc>
2025-09-13 15:18:32 +0300

    add nix derivation

---
+++ pkgs/bsuir-sp.nix
@@ -0,0 +1,25 @@
+{
+  stdenv,
+  cmake,
+  sdl3,
+  sdl3-ttf,
+  fontconfig,
+  ...
+}:
+stdenv.mkDerivation {
+  pname = "bsuir-sp";
+  version = "1.0";
+
+  src = ../.;
+
+  buildInputs = [
+    sdl3
+    sdl3-ttf
+    fontconfig
+  ];
+  nativeBuildInputs = [
+    cmake
+    sdl3.dev
+    fontconfig.dev
+  ];
+}
